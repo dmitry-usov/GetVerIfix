685b57c96e0848bdaa15d3b74c768a24f888fc6c
rmk <53907711+romashkadurashka@users.noreply.github.com>
2020-07-14 22:28:51 +0300

    v0.11 in progress
    upd

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace GetVerIfix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblFixBuild = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnChoose.Location = new System.Drawing.Point(12, 27);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(309, 23);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "choose *.grf file";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Consolas", 26F);
+            this.lblVersion.Location = new System.Drawing.Point(131, 83);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(0, 41);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPath
+            // 
+            this.lblPath.Location = new System.Drawing.Point(12, 53);
+            this.lblPath.MaximumSize = new System.Drawing.Size(309, 50);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(309, 30);
+            this.lblPath.TabIndex = 2;
+            this.lblPath.Text = "Нажми на кнопку выше, чтобы выбрать картинку *.grf";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(333, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Image = global::GetVerIfix.Properties.Resources.github_mark;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(88, 22);
+            this.aboutToolStripMenuItem.Text = "git";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // lblFixBuild
+            // 
+            this.lblFixBuild.AutoSize = true;
+            this.lblFixBuild.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.lblFixBuild.Location = new System.Drawing.Point(109, 124);
+            this.lblFixBuild.Name = "lblFixBuild";
+            this.lblFixBuild.Size = new System.Drawing.Size(0, 13);
+            this.lblFixBuild.TabIndex = 4;
+            this.lblFixBuild.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 145);
+            this.Controls.Add(this.lblFixBuild);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iFIX\'s version";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label lblFixBuild;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace GetVerIfix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog chooseFile = new OpenFileDialog();
+            if (chooseFile.ShowDialog() == DialogResult.OK)
+            {
+                lblPath.Text = chooseFile.FileName;
+            }
+
+            FileStream openFile = new FileStream(chooseFile.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            //DD DD DD.. в числовом виде
+            byte[] etalon = { 221, 221, 221, 221, 221, 221, 221, 221 };
+
+            int counter = 0;
+            FileStream fs = File.OpenRead(chooseFile.FileName);
+            byte[] b = new byte[8];
+
+            while (fs.Read(b, 0, b.Length) > 0)
+            {
+                if (Enumerable.SequenceEqual(b, etalon))
+                {
+                    byte[] m = new byte[8]; //пропускаем 8 байт
+                    byte[] n = new byte[8]; //нужные 8 байт
+                    fs.Read(m, 0, m.Length);
+                    fs.Read(n, 0, n.Length);
+
+                    lblFixBuild.Text = "picture build: " + (n[0] + (n[1] << 8)).ToString();
+                    switch (n[0] + (n[1] << 8))
+                    {
+                        case 5717:
+                            lblVersion.Text = "3.5";
+                            break;
+                        case 6846:
+                            lblVersion.Text = "5.1";
+                            break;
+                        case 10179:
+                            lblVersion.Text = "5.5";
+                            break;
+                        case 10383:
+                            lblVersion.Text = "5.8";
+                            break;
+                        case 10843:
+                            lblVersion.Text = "5.9";
+                            break;
+                        case 11109:
+                            lblVersion.Text = "6.0";
+                            break;
+                        default:
+                            lblVersion.Text = "XZ";
+                            break;
+                    }
+                }
+
+                counter = counter + 8;
+
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Strider757/GetVerIfix");
+        }
+    }
+}
+
+
